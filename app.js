b211239108a9afce01c81e6ee6856de335262f48
Crystal Root <crystal.l.root@gmail.com>
2022-01-03 09:50:53 -0800

    post mongoose population exercise
    with errors

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const passport = require('passport');
-const config = require('./config');
+// const session = require('express-session')
+// const FileStore = require('session-file-store')(session)
+const passport = require('passport')
+// const authenticate = require('./authenticate')
+const config = require('./config')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,13 +21,15 @@ const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-connect.then(() => console.log('Connected correctly to server'), 
+connect.then(() => console.log('Connected correctly to server'),
     err => console.log(err)
 );
+
+
 
 var app = express();
 
@@ -34,18 +40,40 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(passport.initialize());
+// app.use(session({
+//   name: 'session-id',
+//   secret: '12345-67890-09876-54321',
+//   saveUninitialized: false,
+//   resave: false,
+//   store: new FileStore()
+// }))
+
+app.use(passport.initialize())
+// app.use(passport.session())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// const auth = (req, res, next) => {
+//   console.log(req.user)
+//   if(!req.user){
+//       const err = new Error('not a registered user')
+//       err.status = 401
+//       return next(err)
+//   } else {
+//       return next()
+//   }
+// }
+
+// app.use(auth)
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/campsites', campsiteRouter);
-app.use('/partners', partnerRouter);
 app.use('/promotions', promotionRouter);
-
+app.use('/partners', partnerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,7 +86,7 @@ app.use(function(err, req, res, next) {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+    // render the error page
     res.status(err.status || 500);
     res.render('error');
 });
--- authenticate.js
+++ authenticate.js
@@ -1,18 +1,17 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/user');
-
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('./models/user')
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 const config = require('./config.js');
 
-exports.local = passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+exports.local = passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
-exports.getToken = function(user) {
+exports.getToken = user => {
     return jwt.sign(user, config.secretKey, {expiresIn: 3600});
 };
 
